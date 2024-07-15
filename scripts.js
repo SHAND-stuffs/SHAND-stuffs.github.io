0736c55753aee48525c869aed043005125962abb
SH4ND <miqueiasantos20@gmail.com>
2024-07-15 16:23:12 -0300

    DigitalNet: Introduce slideshow
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- scripts.js
+++ scripts.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
